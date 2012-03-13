7a0c13f0e13ee1802efed0f581331da35a6d360c
Ori Ben Dor <ori@nite.org.il>
2012-03-13 14:44:58 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleApp::Application.config.secret_token = 'e20b64b2ae9fcbd825d258101dd9c6bc6c0f74a02183c5c147c490810fcad5c24bbc141c0fa19a8a9738b56e7ea55dd2dc93015e6a38182e80b345836dadbf15'
